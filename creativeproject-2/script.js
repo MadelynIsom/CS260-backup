a561a0d453a3c4b27d4cf32e158c9693ea3c1981
Madelyn Isom <madelynisom@gmail.com>
2022-10-14 02:56:20 +0000

    basic html and js

--- creativeproject-2/script.js
+++ creativeproject-2/script.js
@@ -1,7 +1,77 @@
-document.getElementById("color-code").addEventListener("click", function(event) {
+const targetDiv = document.getElementById("color-results");
+targetDiv.style.display = "none";
+
+
+//generate color randomly
+document.getElementById("generate-color").addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    targetDiv.style.display = "block";
+    
+    let url = "";
+    let hue = document.getElementById("generate-color-by-hue").value;
+    
+    if(hue === "") {
+        url = "https://x-colors.herokuapp.com/api/random";
+    }
+    else {
+        url = "https://x-colors.herokuapp.com/api/random/" + hue;
+    }
+    
+    fetch(url) 
+        .then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            console.log(json);
+            let hexCode = json.hex;
+            let rgbCode = json.rgb;
+            let hslCode = json.hsl;
+            console.log(hexCode);
+            console.log(rgbCode);
+            console.log(hslCode);
+            
+            document.getElementById("color-box").style.backgroundColor = hexCode;
+            
+            document.getElementById("hex-code").innerHTML = hexCode;
+            document.getElementById("rgb-code").innerHTML = rgbCode;
+            document.getElementById("hsl-code").innerHTML = hslCode;
+        });
+});
+
+//convert color code
+document.getElementById("convert").addEventListener("click", function(event) {
   event.preventDefault();
-  const value = document.getElementById("color-code").value;
-  if (value === "")
+  const code = document.getElementById("color-code").value;
+  if (code === "")
     return;
-  console.log(value);
+  console.log(code);
+  
+  let s = document.getElementById('selector');
+  let conversion = s.options[s.selectedIndex].value;
+  
+  let url = "https://x-colors.herokuapp.com/api/" + conversion + "?value=" + code;
+  
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+        console.log(json);
+        
+        switch(conversion){
+            case "hex2rgb":
+            case "hsl2rgb":
+                document.getElementById("conversion-results").innerHTML = json.rgb;
+                break;
+            case "rgb2hex":
+            case "hsl2hex":
+                document.getElementById("conversion-results").innerHTML = json.hex;
+                break;
+            case "hex2hsl":
+            case "rgb2hsl":
+                document.getElementById("conversion-results").innerHTML = json.hsl;
+                break;
+        }
+        
+        
+    });
 });
